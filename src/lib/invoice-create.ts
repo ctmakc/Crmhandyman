17b5fad411c03c950b3a090b6492940918886385
ctmakc <111569641+ctmakc@users.noreply.github.com>
2026-08-15 19:57:13 -0400

    test: make invoice numbering helper directly runnable

--- src/lib/invoice-create.ts
+++ src/lib/invoice-create.ts
@@ -1,5 +1,5 @@
 import { Prisma } from "@prisma/client";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "./prisma";
 
 export type NumberedInvoiceData = Omit<
   Prisma.InvoiceUncheckedCreateInput,
